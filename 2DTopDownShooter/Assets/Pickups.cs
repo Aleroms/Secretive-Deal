281a41854c17dafd2015cd09dc53b79f44d825d4
Jafke104 <eddevera@uci.edu>
2018-11-02 12:28:11 -0700

    Ninja prefab made

---
+++ 2DTopDownShooter/Assets/Pickups.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour {
+
+    PickupsManager pickup;
+	// Use this for initialization
+	void Start () {
+        pickup = gameObject.GetComponent<PickupsManager>();
+        pickup.Pickups = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Jump")){
+            if(pickup.Pickups > 0){
+                Debug.Log("Used Pickup");
+                pickup.Pickups--;
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Untagged"){
+
+            pickup.Pickups++;
+            Debug.Log("Pickup Attained");
+        }
+    }
+}
---
+++ 2DTopDownShooter/Assets/PickupsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupsManager : MonoBehaviour {
+
+    private int pickups;
+    public int Pickups{
+        get;
+        set;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
