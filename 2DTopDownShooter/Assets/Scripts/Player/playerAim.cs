877d12c58e600a54ac32c1b1cda8b78bbcb393fd
Yumpman <lopezeadrian1117@gmail.com>
2018-11-02 15:32:14 -0700

    Added rotation script

---
+++ 2DTopDownShooter/Assets/Scripts/Player/playerAim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAim : MonoBehaviour {
+    float VertRotation;
+    float HorizRotation; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        VertRotation = Input.GetAxis("Vertical Aim");
+        HorizRotation = Input.GetAxis("Horizontal Aim");
+        if(VertRotation != 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90 * VertRotation);
+        }
+        if(HorizRotation < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180 * HorizRotation);
+
+        } else if(HorizRotation > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        
+	}
+}
