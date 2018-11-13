e05fd2c9680cfeb98c05756cf7095eeb8309677a
Santiago Morales <31462534+Aleroms@users.noreply.github.com>
2018-11-12 19:23:50 -0800

    added a main menu scene, buttons and pictures

---
+++ 2DTopDownShooter/Assets/Scripts/UI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+	private int health = 5;
+	public Text healthText;
+
+	void Update()
+	{
+		healthText.text = "Robots saved : " + health;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			health--;
+		}
+	}
+}
